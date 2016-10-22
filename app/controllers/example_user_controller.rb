be19535e9caa31add06cb7ecd7fcea6950a3487d
Mel <meld_castlebar@hotmail.co>
2016-10-22 19:15:04 +0000

    Upload changes

---
+++ app/controllers/example_user_controller.rb
@@ -0,0 +1,15 @@
+#class ExampleUserController < ApplicationController
+  
+  class User
+    attr_accessor :name, :email
+    
+    def initialize(attributes = {})
+      @name = attributes[:name]
+      @email = attributes[:email]
+    end
+    
+    def formatted_email
+      "#{@name} <#{@email}>"
+    end
+  end
+#end
---
+++ test/controllers/example_user_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ExampleUserControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
