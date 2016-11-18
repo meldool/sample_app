d98cc7756aba653289c0af1330f2aa33aae7de83
Mel <meld_castlebar@hotmail.co>
2016-11-18 11:32:30 +0000

    Committing changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  
   root 'static_pages#home' # The main page is set to to the Home page.
   
   #get 'static_pages/home' => Will return Home page if /static_pages/home is entered.
-  #get  '/home',    to: 'static_pages#home' => Will return Home page if /home is entered.
+  # Will return a page below if /page_name is entered i.e. /help.
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
